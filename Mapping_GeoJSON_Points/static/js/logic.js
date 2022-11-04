2a2d12196b67fa1db7a73fac5c04a45d24a89b6f
MarcelinGian <105818879+MarcelinGian@users.noreply.github.com>
2022-11-04 00:14:42 -0700

    Update logic.js

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -108,7 +108,7 @@ let map = L.map('mapid', {
 L.control.layers(baseMaps).addTo(map);
 
 // Accessing the airport GeoJSON URL
-let airportData = "https://raw.githubusercontent.com/CelineWW/Mapping_Earthquakes/main/majorAirports.json";
+let airportData = "https://raw.githubusercontent.com/MarcelinGian/Mapping_Earthquakes/main/majorAirports.json";
 
 // Grabbing our GeoJSON data.
 d3.json(airportData).then(function(data) {
